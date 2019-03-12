8bbc4fd05abdf54bf30c9f3df48603730e038b35
notintheup <brianjandron@gmail.com>
2019-03-11 20:23:59 -0400

    rating filter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,10 @@
 var topics = ["code"];
 // console.log(topics);
 //Function to render anime cards
-function renderAnime() {
-  $(".table-view").empty();
+
+/** function renderAnime() {
+  var tb = $("#table");
+  $("#anime-view").empty();
   for (var i = 0; i < topics.length; i++) {
     var a = $("<td>");
     a.addClass("auto-td");
@@ -13,54 +15,55 @@ function renderAnime() {
     $("#lookup-value").empty();
   }
 }
-
+*/
 //Function to display anime 
 function displayAnime() {
 
   // var search_anime = $(this).attr("name");
-  var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + topics + "&limit=4";
+  var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + topics + "&limit=10";
   // console.log(queryURL)
-
+  $("#anime-view").empty();
   // Ajax call to API
   $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  .done(function (response) {
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function (response) {
 
-    for (var i = 0; i < response.results.length; i++) {
-      console.log(response.results[i])
-      var animeURL = response.results[i].image_url;
-      var table = $("<table>");
-      var animeDiv = $("<td class= row>");
-      var newImg = $("<img>");
-      newImg.attr("src", animeURL);
-      newImg.attr("rated", response.results[i].rated)
-      newImg.attr("title", response.results[i].title)
-      newImg.attr("synopsis", response.results[i].synopsis)
-      newImg.attr("url", response.results[i].url)
-      newImg.addClass("image");
-      table.append(animeDiv);
-      animeDiv.append(newImg);
-      var title = $("<h2>").text("Title: " + response.results[i].title);
-      var ratings = $("<h3>").text("rated: " + response.results[i].rated);
-      var synopsis = $("<p>").text("Synopsis: " + response.results[i].synopsis);
-      var url = $("<a href>").text(response.results[i].url);
-      animeDiv.append(title, [ratings, synopsis, url]);
-      $("#anime-view").append(animeDiv);
+      for (var i = 0; i < response.results.length; i++) {
+        if (response.results[i].rated !== 'R'){
+        var animeURL = response.results[i].image_url;
+        var table = $("<table id=table>");
+        var animeDiv = $("<td class = row >");
+        var imageDiv = $("<div class = imagebox>");
+        var contentDiv = $("<div class = content>");
+        var newImg = $("<img class = img>");
+        newImg.attr("src", animeURL);
+        newImg.attr("rated", response.results[i].rated);
+        newImg.attr("title", response.results[i].title);
+        newImg.attr("synopsis", response.results[i].synopsis);
+        newImg.addClass("image");
+        table.append(animeDiv);
+        // animeDiv.append(newImg);
 
+        var ratings = $("<p class= text-format>").text("Rating: " + response.results[i].rated);
+        var title = $("<p class= text-format>").text("Title:  " + response.results[i].title);
+        var synopsis = $("<p class= text-format>").text("Synopsis:  " + response.results[i].synopsis);
+        imageDiv.append(newImg);
+        contentDiv.append(title, ratings, synopsis);
+        animeDiv.append(imageDiv, contentDiv); 
+        $("#anime-view").append(animeDiv);
+
+      }
     }
-  })
+    });
 }
 //Click handlers start now
 $("#search-otaku").on("click", function (event) {
   event.preventDefault();
   var lookup = $("#lookup-value").val().trim();
-  topics.push(lookup);
-  // console.log(lookup);
+  topics = lookup;
+  // topics.push(lookup);
   displayAnime();
 })
-// Handler of animation
-// $(document).on("click", ".image", detailsAnime);
-
 displayAnime();
