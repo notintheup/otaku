b61716774ae7c37c4989b04efc78b21deffab861
Stephen <43526838+wikipockets@users.noreply.github.com>
2019-03-13 20:35:29 -0400

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 // Otaku javascript starts now
-var topics = ["code"];
-// console.log(topics);
+var topics = ["turtle"];
+
 //Function to display anime 
 function displayAnime() {
 
   // var search_anime = $(this).attr("name");
-  var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + topics + "&limit=5";
+  var queryURL = "https://api.jikan.moe/v3/search/anime?q=" + topics + "&limit=8";
   // console.log(queryURL)
   $("#anime-view").empty();
   // Ajax call to API
@@ -17,38 +17,57 @@ function displayAnime() {
 
       for (var i = 0; i < response.results.length; i++) {
         console.log(response.results[i]);
-        console.log(response.results[i].synopsis);
-        var animeURL = response.results[i].image_url;
-        var table = $("<table id=table>");
-        var animeDiv = $("<td class = row >");
-        var imageDiv = $("<div class = imagebox>");
-        var contentDiv = $("<div class = content>");
-        var newImg = $("<img class = img>");
-        newImg.attr("src", animeURL);
-        newImg.attr("rated", response.results[i].rated);
-        newImg.attr("title", response.results[i].title);
-        newImg.attr("synopsis", response.results[i].synopsis);
-        newImg.addClass("image");
-        table.append(animeDiv);
-
-        var ratings = $("<p class= text-format>").text("Rating: " + response.results[i].rated);
-        var title = $("<p class= text-format>").text("Title:  " + response.results[i].title);
-        var synopsis = $("<p class= text-format>").text("Synopsis:  " + response.results[i].synopsis);
-        imageDiv.append(newImg);
-        contentDiv.append(title, ratings, synopsis);
-        animeDiv.append(imageDiv, contentDiv);
-        $("#anime-view").append(animeDiv);
-
+        if (response.results[i].rated !== 'R') {
+          var animeURL = response.results[i].image_url;
+          var table = $("<table id=table>");
+          var animeDiv = $("<td class=card>");
+          var imageDiv = $("<div class = card-body>");
+          var contentDiv = $("<div class = card-text>");
+          var newImg = $("<img class = card-img-top>");
+          newImg.attr("src", animeURL);
+          newImg.attr("rated", response.results[i].rated);
+          newImg.attr("title", response.results[i].title);
+          newImg.attr("synopsis", response.results[i].synopsis);
+          newImg.attr("url", response.results[i].url);
+          table.append(imageDiv);
+          var ratings = $("<p class= text-format>").text("Rating: " + response.results[i].rated);
+          var title = $("<p class= text-format>").text("Title:  " + response.results[i].title);
+          var synopsis = $("<p class= text-format>").text("Synopsis:  " + response.results[i].synopsis);
+          imageDiv.append(newImg, title, ratings, synopsis);
+          animeDiv.append(imageDiv, contentDiv);
+          $("#anime-view").append(animeDiv);
+        }
       }
     });
 }
+//Wikipedia search button
+$("#searchWiki").click(function () {
+  var q = document.getElementById("searchid").value;
+  $('#results').html('');
+  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=5&generator=search&origin=*&gsrsearch=" + q, function (data) {
+    $('#results').append('<h2>Top 5 Wiki Search Results for "' + q + '"</h2>');
+    $.each(data.query.pages, function (i) {
+      $('#results').append("<p><a href='https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid +
+        "' target='_blank'>" + data.query.pages[i].title + "</a></p>");
+    });
+
+  });
+});
+
+$("#clearSearch").click(function () {
+  document.getElementById("searchid").value = "";
+  document.getElementById("results").innerHTML = "";
+});
+
 //Click handlers start now
 $("#search-otaku").on("click", function (event) {
   event.preventDefault();
   var lookup = $("#lookup-value").val().trim();
   topics = lookup;
-  // topics.push(lookup);
-  // console.log(lookup);
   displayAnime();
+})
+$(".img").on("click", function (event) {
+  event - preventDefault();
+
 })
 displayAnime();
